8fa2b48698e9fdcd3660223ecaf30f34f0614f41
Alex Fabiano Baptista da Silva Junior <alexfabianojr@gmail.com>
2020-07-02 04:15:02 -0300

    Get All, Get by Id e Post

---
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -0,0 +1,31 @@
+package com.apirest.webflux.controller;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.services.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PlaylistController {
+
+    @Autowired
+    PlaylistService playlistService;
+
+    @GetMapping(value = "/playlist")
+    public Flux<Playlist> getPlaylist() {
+        return playlistService.findAll();
+    }
+
+    @GetMapping(value = "/playlist/{id}")
+    public Mono<Playlist> getPlaylistId(@PathVariable String id) {
+        return playlistService.findById(id);
+    }
+
+    @PostMapping(value = "/playlist")
+    public Mono<Playlist> save(@RequestBody Playlist playlist) {
+        return playlistService.save(playlist);
+    } /*Anotação requestbody para o banco saber que ele está recebendo um objeto playlist em formato JSON*/
+
+}
